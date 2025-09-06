2107ddb863a4708662cbdc18e622e4059f733ea6
shanthini91 <100746521+shanthini91@users.noreply.github.com>
2025-09-06 12:25:23 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,59 @@
 $(document).ready(function() {
-  // Toggle menu
-  $('.hamburger-menu').click(function() {
-    $('.navigation').toggleClass('change');
+    lightbox.option({
+        'wrapAround': true
+      });
+
+  // =========================
+  // Hamburger Menu Toggle
+  // =========================
+  $(".hamburger-menu").click(function () {
+    $(".navigation").toggleClass("change");
   });
 
   // Close menu on nav link click
-  $('.link-item').click(function() {
-    $('.navigation').removeClass('change');
+  $(".nav-link").click(function () {
+    $(".navigation").removeClass("change");
   });
 
-  // Lightbox
-  lightbox.option({
-    'wrapAround': true
-  });
+      $('.writers-accordion').click(function(event) {
+        
+        if(event.target.id.split('-')[0] === 'button') {
+           $('#book-1').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book1.jpg');
+           $('#book-2').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
+        }
 
-  // Gallery animation
-  $(window).scroll(function() {
-    let position = $(this).scrollTop();
-    if (position >= 350) {
-      $('.gallery').addClass('change');
-    } else {
-      $('.gallery').removeClass('change');
-    }
-  });
-
-  // Writers accordion
-  $('.writers-accordion').click(function(event) {
-    if (event.target.id.split('-')[0] === 'button') {
-      $('#book-1').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book1.jpg');
-      $('#book-2').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
-    }
-  });
+      });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
