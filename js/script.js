3585b40bce1dfb3b2bab792595a8ba8807e06aff
shanthini91 <100746521+shanthini91@users.noreply.github.com>
2025-09-06 13:01:58 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,34 +1,58 @@
-$(document).ready(function () {
-  // Lightbox settings
-  lightbox.option({
-    wrapAround: true
-  });
+$(document).ready(function() {
+    lightbox.option({
+        'wrapAround': true
+      });
 
-  // Gallery scroll effect
-  $(window).scroll(function () {
-    let position = $(this).scrollTop();
+      $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        
+        if(position >= 350) {
+            $('.gallery').addClass('change');
+        } else {
+            $('.gallery').removeClass('change');
+        }
 
-    if (position >= 350) {
-      $(".gallery").addClass("change");
-    } else {
-      $(".gallery").removeClass("change");
-    }
-  });
+      });
 
-  // Writers accordion - updates book images dynamically
-  $(".writers-accordion").click(function (event) {
-    let idParts = event.target.id.split("-");
-    if (idParts[0] === "button") {
-      let writer = idParts[1];
-      $("#book-1").attr("src", "img/writers/" + writer + "-book1.jpg");
-      $("#book-2").attr("src", "img/writers/" + writer + "-book2.jpg");
-    }
-  });
+      $('.writers-accordion').click(function(event) {
+        
+        if(event.target.id.split('-')[0] === 'button') {
+           $('#book-1').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book1.jpg');
+           $('#book-2').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
+        }
 
-  // Hamburger menu toggle
-  $(".hamburger-menu").click(function () {
-    $(this).toggleClass("change");
-    $(".bg-cover").toggleClass("change");
-    $(".nav-content").toggleClass("change");
-  });
+      });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
