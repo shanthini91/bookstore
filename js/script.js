4ab923c055e86d87b8e2d1326931162d8328719a
shanthini91 <100746521+shanthini91@users.noreply.github.com>
2025-09-06 13:52:06 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -1,49 +1,32 @@
-$(document).ready(function() {
+$(document).ready(function () {
     lightbox.option({
         'wrapAround': true
-    });
 
-    // Gallery scroll animation
-    $(window).scroll(function() {
-        let position = $(this).scrollTop();
-        
-        if(position >= 350) {
+    });
+    $(window).scroll(function () {
+        let position = $(this).scrollTop(); 
+        if (position >= 350) {
             $('.gallery').addClass('change');
-        } else {
-            $('.gallery').removeClass('change');
+        } else { 
+            $('.gallery').removeClass('change'); 
         }
     });
-
-    // Hamburger icon animation & bg-cover scale
-    $(".hamburger-menu").click(function() {
-        $(this).toggleClass("change");
-        $(".bg-cover").toggleClass("change");
-    });
-
-    // Smooth scroll & close menu
-    $(".nav-content .link-item").click(function(e) {
-        e.preventDefault();
-
-        let target = $(this).attr("href");
-
-        // Animate scroll to section
-        $("html, body").animate({
-            scrollTop: $(target).offset().top
-        }, 800);
-
-        // Close Bootstrap collapse
-        $('#myNavbar').collapse('hide');
-
-        // Reset hamburger icon & bg-cover
+    $(".hamburger-menu").click(function () 
+                                   {
+                                       $(this).toggleClass("change"); 
+                                       $(".bg-cover").toggleClass("change"); 
+                                       $(".nav-content").toggleClass("change"); 
+                                   });
+    // Close menu when any link is clicked //
+    $(".nav-content .link-item").click(function () {
         $(".hamburger-menu").removeClass("change");
         $(".bg-cover").removeClass("change");
+        $(".nav-content").removeClass("change");
     });
-
-    // Writers accordion
-    $('.writers-accordion').click(function(event) {
-        if(event.target.id.split('-')[0] === 'button') {
-           $('#book-1').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book1.jpg');
-           $('#book-2').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
+    $('.writers-accordion').click(function (event) {
+        if (event.target.id.split('-')[0] === 'button') {
+            $('#book-1').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book1.jpg');
+            $('#book-2').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
         }
     });
 });
