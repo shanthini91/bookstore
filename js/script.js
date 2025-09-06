88df5d48b2ee684f407988ba47f56fcf40bd008d
shanthini91 <100746521+shanthini91@users.noreply.github.com>
2025-09-06 12:28:11 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,58 @@
-$(document).ready(function () {
-  // =========================
-  // Hamburger Menu Toggle
-  // =========================
-  $(".hamburger-menu").click(function () {
-    $(".navigation").toggleClass("change");
-  });
+$(document).ready(function() {
+    lightbox.option({
+        'wrapAround': true
+      });
 
-  // Close menu on nav link click
-  $(".nav-link").click(function () {
-    $(".navigation").removeClass("change");
-  });
+      $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        
+        if(position >= 350) {
+            $('.gallery').addClass('change');
+        } else {
+            $('.gallery').removeClass('change');
+        }
+
+      });
+
+      $('.writers-accordion').click(function(event) {
+        
+        if(event.target.id.split('-')[0] === 'button') {
+           $('#book-1').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book1.jpg');
+           $('#book-2').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
+        }
+
+      });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
