0fcc19b8d11d888031f2f6033c5d7f6623fad1ea
shanthini91 <100746521+shanthini91@users.noreply.github.com>
2025-09-06 13:38:01 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
     lightbox.option({
         'wrapAround': true
-      });
+    });
 
-      $(window).scroll(function() {
+    // Gallery scroll animation
+    $(window).scroll(function() {
         let position = $(this).scrollTop();
         
         if(position >= 350) {
@@ -11,63 +12,38 @@ $(document).ready(function() {
         } else {
             $('.gallery').removeClass('change');
         }
+    });
 
-      });
- $(".hamburger-menu").click(function() {
+    // Hamburger icon animation & bg-cover scale
+    $(".hamburger-menu").click(function() {
         $(this).toggleClass("change");
         $(".bg-cover").toggleClass("change");
-        $(".nav-content").toggleClass("change");
     });
 
-    // Close menu when any link is clicked
-    $(".nav-content .link-item").click(function() {
+    // Smooth scroll & close menu
+    $(".nav-content .link-item").click(function(e) {
+        e.preventDefault();
+
+        let target = $(this).attr("href");
+
+        // Animate scroll to section
+        $("html, body").animate({
+            scrollTop: $(target).offset().top
+        }, 800);
+
+        // Close Bootstrap collapse
+        $('#myNavbar').collapse('hide');
+
+        // Reset hamburger icon & bg-cover
         $(".hamburger-menu").removeClass("change");
         $(".bg-cover").removeClass("change");
-        $(".nav-content").removeClass("change");
     });
 
-      $('.writers-accordion').click(function(event) {
-        
+    // Writers accordion
+    $('.writers-accordion').click(function(event) {
         if(event.target.id.split('-')[0] === 'button') {
            $('#book-1').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book1.jpg');
            $('#book-2').attr('src', 'img/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
         }
-
-      });
+    });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
